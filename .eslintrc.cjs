63914b4cfe2804f62f2370c8fb48afc45ac089d5
ydroal <droal.yoko@gmail.com>
2023-05-12 16:02:16 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'space-before-function-paren': 'never',
+        semi: true
+      }
+    ]
+  }
+};
